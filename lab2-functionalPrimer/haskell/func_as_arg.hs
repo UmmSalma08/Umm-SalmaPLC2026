5fbc4ff6e91c1a0d3fc57c81c7447fc77e9609cc
Umm Salma Hamisu <240132950@aston.ac.uk>
2026-01-31 10:03:03 +0000

    Update applicatorFunc to handle range inputs
    Refactor applicatorFunc to accept range and condition.

--- lab2-functionalPrimer/haskell/func_as_arg.hs
+++ lab2-functionalPrimer/haskell/func_as_arg.hs
@@ -1,9 +1,12 @@
 --create inpFunc
-inpFunc = [1..1000] 
+inpFunc = [a..b] 
 
 --Define applicatorFunc
-applicatorFunc inpFunc s = if s=='s' then sum inpFunc else (sum inpFunc)/5  
+applicatorFunc inpFunc a b s = if s=='s' then sum inpFunc a b else (sum inpFunc a b)/(b - a+1)
+
+applicatorFunc a b s | s == "s" = sum inpFunc a b
+                     | otherwise (sum inpFunc a b)/(b - a+1)
 
 main = do
-    let result = applicatorFunc inpFunc 'a' --Call applicatorFunc with inpFunc and 'a' as args
+    let result = applicatorFunc inpFunc 2, 20 's' --Call applicatorFunc with inpFunc and 'a' as args
     putStrLn("sum = " ++ show(result))
