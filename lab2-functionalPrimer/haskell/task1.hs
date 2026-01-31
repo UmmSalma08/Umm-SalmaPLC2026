cb78f115f55c0a319eca4c495043e13387423ea4
Umm Salma Hamisu <240132950@aston.ac.uk>
2026-01-31 09:27:03 +0000

    Refactor functions and variable assignments in task1.hs
    Updated function definitions and variable assignments.

--- lab2-functionalPrimer/haskell/task1.hs
+++ lab2-functionalPrimer/haskell/task1.hs
@@ -1,11 +1,13 @@
 module Main where
 
+f1 = print(x * 10)
+x = 5
+f2 = print(x * 2)
+x = 10
+myFunc3 = "Hello, this is 3rd Func"
+
 main =
     do
     f1
     f2 
     putStrLN myFunc3
-
-f1 x = x*10 where x = 5
-f2 x = x*2 where x = 10
-myFunc3 = "Hello, this is 3rd Func"
