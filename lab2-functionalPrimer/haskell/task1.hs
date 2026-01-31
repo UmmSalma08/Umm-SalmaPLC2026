1f8014a9c8eb8a11f96e4b69990350434e251323
Umm Salma Hamisu <240132950@aston.ac.uk>
2026-01-31 09:21:40 +0000

    Refactor function names and update main execution

--- lab2-functionalPrimer/haskell/task1.hs
+++ lab2-functionalPrimer/haskell/task1.hs
@@ -2,10 +2,10 @@ module Main where
 
 main =
     do
-    print myFunc1
-    print MyFunc2 
+    f1
+    f2 
     putStrLN myFunc3
 
-myFunc1 x = x*10 where x = 5
-MyFunc2 x = x*2 where x = 10
+f1 x = x*10 where x = 5
+f2 x = x*2 where x = 10
 myFunc3 = "Hello, this is 3rd Func"
