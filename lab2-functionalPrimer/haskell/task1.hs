16fe3f9da8cff7631d81d9d7aea27aa0aed9895b
Umm Salma Hamisu <240132950@aston.ac.uk>
2026-01-31 09:20:10 +0000

    Refactor function calls and definitions in task1.hs
    Updated function calls and definitions in task1.hs.

--- lab2-functionalPrimer/haskell/task1.hs
+++ lab2-functionalPrimer/haskell/task1.hs
@@ -2,10 +2,10 @@ module Main where
 
 main =
     do
-    print (myFunc1 5)
-    print (MyFunc2 10)
+    print myFunc1
+    print MyFunc2 
     putStrLN myFunc3
 
-myFunc1 x = x*10
-MyFunc2 x = x*2
+myFunc1 x = x*10 where x = 5
+MyFunc2 x = x*2 where x = 10
 myFunc3 = "Hello, this is 3rd Func"
